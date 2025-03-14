16bbd7b39ac2667fccb386c132571a519c619507
zixuan-liu-17 <zixuan.liu@proton.me>
2025-03-14 15:01:46 +0000

    update infomation

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg
 import { } from '@fortawesome/free-solid-svg-icons';
 
 export const navigation = {
-  name: "Hashir",
+  name: "Danyang",
   links: [
     {
       title: "About",
@@ -29,8 +29,9 @@ export const navigation = {
   ],
 }
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
+  title: "Hey, I'm Danyang",
+  description: ["My research broadly centres on romantic relationships.",
+    "\nI am passionate about using psychology to help people live better lives."],
   image: profile.src,
   buttons: [
     {
@@ -38,34 +39,46 @@ export const intro = {
       link: "#contact",
       isPrimary: true,
     },
-    {
-      title: "Resume",
-      link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-      isPrimary: false,
-    },
+    // {
+    //   title: "Resume",
+    //   link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
+    //   isPrimary: false,
+    // },
   ],
 }
 
 export const about = {
-  title: "Who I am",
+  title: "About Me",
   description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
+    "I am a Postdoctral Researcher at the University of Birmingham, UK. I graduated from the University of Bristol in 2024 with a Doctral degree in Social Psychology. I'm a certified therapist, National Career Development Association (NCDA) career development practitioner, and under training of Emotionally Focused Therapy.",
+    "My research broadly centres on the processes and mechanisms that drive adult attachment. Briefly, on the basis of information abstracted from consistent and long-term attachment relationships, individuals develop mental models of attachment, or attachment styles. A key feature of attachment styles is that they predictor how individuals respond to threat and deal with stress and distress. Much of my research over the years has been concerned with the way that attachment styles and attachment-relevant hormones influence thought, behaviour and feelings in interpersonal relationships and social life more generally.",
+    "My current research projects focus on (1) developing digital health interventions for couples, especially on reducing negative attribution towards partners; (2) evaluating whether we can use chatbots to make people feel 'secure' in love; (3) exploring online dating patterns. If you’re interested in hearing more about my research, please navigate to the research page.",
+    "While I currently primarily work in research, I continue to see clients remotely. I work with individuals and couples. I specialise in self exploration, communication problems in relationship and attribution intervention."
   ],
 }
 
 export const work = {
-  title: "What I do",
+  title: "Research Projects",
   cards: [
     {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+      title: "Whether we correctly interpret partners?",
+      description: ["Insecure attachment orientation in adults and children and negative attribution bias: A meta-analysis. https://doi.org/10.1177/01461672221117690",
+        "Validation of a new assessment of hostile attribution bias in romantic relationships: The Hostile Attributions in Romantic Relationships Test. https://doi.org/10.1111/jmft.12694",
+        "",
+        "How hostile attribution bias affects attachment insecurity and relationship satisfaction during COVID pandemic, from the cross-cultural perspective",
+        "",
+        "Attachment orientations bias the attentional processing of images of relationship-relevant interactions, moderated by hostile attribution bias.",
+        "",
+        "Feeling Secure Affects the Way You Interpret the World. SPSP Community. https://spsp.org/news/character-and-context-blog/li-attachment-attribution-style-feelings-of-security",
+        "",
+        "Insecure attachment linked to a psychological phenomenon known as negative attribution bias. The PsyPost. https://www.psypost.org/insecure-attachment-linked-to-a-psychological-phenomenon-known-as-negative-attribution-bias/"],
+
       icons: null,
     },
     {
-      title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
+      title: "Disappeared lover",
+      description: ["Do Adult Attachment Orientations Influence the Experience of Ghosting or Being Ghosted?",
+        "How secure priming of adult attachment affects the Ghosting-related behaviours in romantic contexts."],
       icons: null,
     }
   ],
